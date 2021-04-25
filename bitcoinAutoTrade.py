6c583369bcc3c8e95f47cdb07eac31699ce2d3d7
dlawogus95 <jh951021@gmail.com>
2021-04-25 22:22:36 +0900

    Update bitcoinAutoTrade.py

--- bitcoinAutoTrade.py
+++ bitcoinAutoTrade.py
@@ -56,24 +56,24 @@ post_message(myToken,"#crypto", "autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time("KRW-DOGE")
         end_time = start_time + datetime.timedelta(days=1)
         time.sleep(1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            ma15 = get_ma15("KRW-BTC")
-            current_price = get_current_price("KRW-BTC")
+            target_price = get_target_price("KRW-DOGE", 0.4)
+            ma15 = get_ma15("KRW-DOGE")
+            current_price = get_current_price("KRW-DOGE")
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
-                    post_message(myToken,"#crypto", "BTC buy : " +str(buy_result))
+                    buy_result = upbit.buy_market_order("KRW-DOGE", krw*0.9995)
+                    post_message(myToken,"#crypto", "DOGE buy : " +str(buy_result))
         else:
-            btc = get_balance("BTC")
+            btc = get_balance("DOGE")
             if btc > 0.00008:
-                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
-                post_message(myToken,"#crypto", "BTC buy : " +str(sell_result))
+                sell_result = upbit.sell_market_order("KRW-DOGE", btc*0.9995)
+                post_message(myToken,"#crypto", "DOGE buy : " +str(sell_result))
        
     except Exception as e:
         print(e)
